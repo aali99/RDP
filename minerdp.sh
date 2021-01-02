597a35933fc76651404cd192148f35ee67417d66
aali99 <68443067+aali99@users.noreply.github.com>
2021-01-02 14:45:41 +0200

    Update minerdp.sh

--- minerdp.sh
+++ minerdp.sh
@@ -1,7 +1,7 @@
 
 printf "Installing RDP Be Patience... " >&2
 {
-
+sudo su
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
@@ -21,78 +21,77 @@ sudo apt install nautilus nano -y
 toolsDir="/opt"
 echo "[*] INSTALLING DEPENDENCIES IN \"$toolsDir\"..."
 
-mkdir -p "$toolsDir"
+sudo mkdir -p "$toolsDir"
 
-apt install -y phantomjs xvfb dnsutils nmap
+sudo apt install -y phantomjs xvfb dnsutils nmap
 
 echo "[*] INSTALLING GO DEPENDENCIES (OUTPUT MAY FREEZE)..."
 
-wget -q -O - https://git.io/vQhTU | bash
-sleep 2
-source ~/.bashrc
-cd /usr/local
-wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go get github.com/jaeles-project/jaeles
-go get -u github.com/KathanP19/Gxss
-go get -u github.com/lc/gau
-go get -u github.com/tomnomnom/gf
-go get -u github.com/jaeles-project/gospider
-go get -u github.com/projectdiscovery/httpx/cmd/httpx
-go get -u github.com/tomnomnom/qsreplace
-go get -u github.com/haccer/subjack
-go get -u github.com/tomnomnom/assetfinder
-go get github.com/hakluke/hakrawler
-go get -u github.com/OWASP/Amass/v3/...
-go get -u github.com/ffuf/ffuf
-go get -u github.com/ethicalhackingplayground/bxss
-git clone https://github.com/projectdiscovery/httpx.git; cd httpx/cmd/httpx; go build; mv httpx /usr/local/bin/; httpx -version
-cd $GOPATH/src/github.com/OWASP/Amass
-go install ./...
+sudo wget -q -O - https://git.io/vQhTU | bash
+sudo sleep 2
+sudo source ~/.bashrc
+sudo cd /usr/local
+sudo wget https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.6.linux-amd64.tar.gz
+sudo export PATH=$PATH:/usr/local/go/bin
+sudo go get github.com/jaeles-project/jaeles
+sudo go get -u github.com/KathanP19/Gxss
+sudo go get -u github.com/lc/gau
+sudo go get -u github.com/tomnomnom/gf
+sudo go get -u github.com/jaeles-project/gospider
+sudo go get -u github.com/projectdiscovery/httpx/cmd/httpx
+sudo go get -u github.com/tomnomnom/qsreplace
+sudo go get -u github.com/haccer/subjack
+sudo go get -u github.com/tomnomnom/assetfinder
+sudo go get github.com/hakluke/hakrawler
+sudo go get -u github.com/OWASP/Amass/v3/...
+sudo go get -u github.com/ffuf/ffuf
+sudo go get -u github.com/ethicalhackingplayground/bxss
+sudo git clone https://github.com/projectdiscovery/httpx.git; cd httpx/cmd/httpx; go build; mv httpx /usr/local/bin/; httpx -version
+sudo cd $GOPATH/src/github.com/OWASP/Amass
+sudo go install ./...
 
 
-echo "[*] INSTALLING RUSTSCAN..."
+sudo echo "[*] INSTALLING RUSTSCAN..."
 
-wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
-dpkg -i rustscan_2.0.1_amd64.deb
+sudo wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
+sudo dpkg -i rustscan_2.0.1_amd64.deb
 
-echo "[*] INSTALLING GIT DEPENDENCIES..."
+sudo echo "[*] INSTALLING GIT DEPENDENCIES..."
 
-git clone https://github.com/hahwul/dalfox
-cd dalfox
-go install
-go build
+sudo git clone https://github.com/hahwul/dalfox
+sudo cd dalfox
+sudo go install
+sudo go build
 
-git clone https://github.com/chvancooten/BugBountyScanner
+sudo git clone https://github.com/chvancooten/BugBountyScanner
 ### Nuclei (Workaround -https://github.com/projectdiscovery/nuclei/issues/291)
-cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
-git clone -q https://github.com/projectdiscovery/nuclei.git
-cd nuclei/v2/cmd/nuclei/ || { echo "Something went wrong"; exit 1; }
-go build
-mv nuclei /usr/local/bin/
+sudo cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
+sudo git clone -q https://github.com/projectdiscovery/nuclei.git
+sudo cd nuclei/v2/cmd/nuclei/ || { echo "Something went wrong"; exit 1; }
+sudo go build
+sudo mv nuclei /usr/local/bin/
 
 ### Nuclei templates
-cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
-git clone -q https://github.com/projectdiscovery/nuclei-templates.git
-nuclei --update-templates 
+sudo cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
+sudo git clone -q https://github.com/projectdiscovery/nuclei-templates.git
+sudo nuclei --update-templates 
 ### Gf-Patterns
-cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
-git clone -q https://github.com/1ndianl33t/Gf-Patterns
-mkdir ~/.gf
-cp "$toolsDir"/Gf-Patterns/*.json ~/.gf
+sudo cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
+sudo git clone -q https://github.com/1ndianl33t/Gf-Patterns
+sudo mkdir ~/.gf
+sudo cp "$toolsDir"/Gf-Patterns/*.json ~/.gf
 
-pip3 install telegram-send
+sudo pip3 install telegram-send
 
-echo "[*] SETUP FINISHED."
+sudo echo "[*] SETUP FINISHED."
 
-sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
+
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
